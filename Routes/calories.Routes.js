07cb09a42e1bc4f9065344ed37f4d447b9f59e45
poojakittu <poojayadav.yadav173@gmail.com>
2023-04-08 16:56:25 +0530

    added

--- Routes/calories.Routes.js
+++ Routes/calories.Routes.js
@@ -4,6 +4,7 @@ const authMiddleware = require("../middleware/auth.middleware");
 const caloriesRoutes = express.Router();
 const jwt = require("jsonwebtoken");
 const { caloriesModel } = require("../Model/calories");
+const { Console } = require("winston/lib/winston/transports");
 
 caloriesRoutes.get("/allcalories", async (req, res) => {
   try {
@@ -19,11 +20,14 @@ caloriesRoutes.get("/allcalories", async (req, res) => {
   }
 });
 
-caloriesRoutes.get("/todaycalories",authMiddleware, async (req, res) => {
+caloriesRoutes.get("/todaycalories", authMiddleware, async (req, res) => {
   const x = req.query.q;
 
   try {
-    const product = await caloriesModel.find({ date: x,userId:req.body.userId });
+    const product = await caloriesModel.find({
+      date: x,
+      userId: req.body.userId,
+    });
     res.send({ data: product, total: product.length });
   } catch (error) {
     console.log("error", error);
@@ -34,7 +38,7 @@ caloriesRoutes.get("/todaycalories",authMiddleware, async (req, res) => {
   }
 });
 
-caloriesRoutes.post("/add",authMiddleware, async (req, res) => {
+caloriesRoutes.post("/add", authMiddleware, async (req, res) => {
   let x = new Date();
   let y = JSON.stringify(x);
   let bag = "";
@@ -42,14 +46,69 @@ caloriesRoutes.post("/add",authMiddleware, async (req, res) => {
     bag += y[i];
   }
   let payload = req.body;
-  const date = { date: bag,userId:req.body.userId };
-  // console.log(payload)
+
+  const date = { date: bag, userId: req.body.userId };
+  const data = await caloriesModel.find({ userId: req.body.userId, date: bag });
   try {
-    let data1 = new caloriesModel(payload);
-    // console.log(data1);
-    let saved = await data1.save();
-    await caloriesModel.findByIdAndUpdate({ _id: saved._id }, date);
-    res.send({ msg: "Your item is Added" });
+    if (data.length === 0) {
+      const data1 = new caloriesModel(payload);
+      // Set the "date" field in the payload
+      data1.date = date;
+      // Save the new document to the database
+      const saved = await data1.save();
+      // Update the document's "date" field with the saved document's ID
+      await caloriesModel.findByIdAndUpdate(saved._id, { date: saved._id });
+      res.send({ msg: "Your item is Added" });
+    } else {
+      const data1 = await caloriesModel.findOneAndUpdate({
+        userId: req.body.userId,
+        date: bag,
+      });
+      if (payload.breakfast) {
+        for (let i = 0; i < payload.breakfast.length; i++) {
+          data1.breakfast.push({
+            name: payload.breakfast[i].name,
+            quantity: payload.breakfast[i].quantity,
+            calories: payload.breakfast[i].calories,
+          });
+          await data1.save();
+        }
+      }
+      if (payload.lunch) {
+        for (let i = 0; i < payload.lunch.length; i++) {
+          data1.lunch.push({
+            name: payload.lunch[i].name,
+            quantity: payload.lunch[i].quantity,
+            calories: payload.lunch[i].calories,
+          });
+          await data1.save();
+        }
+      }
+
+      if (payload.eveningsnacks) {
+        for (let i = 0; i < payload.eveningsnacks.length; i++) {
+          data1.eveningsnacks.push({
+            name: payload.eveningsnacks[i].name,
+            quantity: payload.eveningsnacks[i].quantity,
+            calories: payload.eveningsnacks[i].calories,
+          });
+          await data1.save();
+        }
+      }
+
+      if (payload.dinner) {
+        for (let i = 0; i < payload.dinner.length; i++) {
+          data1.dinner.push({
+            name: payload.dinner[i].name,
+            quantity: payload.dinner[i].quantity,
+            calories: payload.dinner[i].calories,
+          });
+          await data1.save();
+        }
+      }
+      res.send({ msg: "Your item is updated" });
+    }
+    
   } catch (err) {
     res.send(err);
   }
@@ -63,110 +122,106 @@ caloriesRoutes.patch("/update/:id", async (req, res) => {
   try {
     const data = await caloriesModel.findById(Id);
     if (val == "breakfast") {
-      let sum=0
-      let total=0
+      let sum = 0;
+      let total = 0;
       data.breakfast.forEach((item) => {
         const x = JSON.stringify(item._id);
         const y = JSON.stringify(foodid);
         if (x == y) {
           item.istrue = "true";
         }
-        if(item.istrue=="true"){
-          sum+=(item.calories);
+        if (item.istrue == "true") {
+          sum += item.calories;
         }
-        total+=(item.calories);
-        
+        total += item.calories;
       });
-      await data.save();    
-      res.send({data:data.breakfast,consumption:sum,total:total});
-    }  else if (val == "lunch") {
-      let sum=0
-      let total=0
+      await data.save();
+      res.send({ data: data.breakfast, consumption: sum, total: total });
+    } else if (val == "lunch") {
+      let sum = 0;
+      let total = 0;
       data.lunch.forEach((item) => {
         const x = JSON.stringify(item._id);
         const y = JSON.stringify(foodid);
         if (x == y) {
           item.istrue = "true";
         }
-        if(item.istrue=="true"){
-          sum+=(item.calories);
+        if (item.istrue == "true") {
+          sum += item.calories;
         }
-        total+=(item.calories);
-        
+        total += item.calories;
       });
       await data.save();
-      res.send({data:data.lunch,consumption:sum,total:total});
+      res.send({ data: data.lunch, consumption: sum, total: total });
     } else if (val == "eveningsnacks") {
-      let sum=0
-      let total=0
+      let sum = 0;
+      let total = 0;
       data.eveningsnacks.forEach((item) => {
         const x = JSON.stringify(item._id);
         const y = JSON.stringify(foodid);
         if (x == y) {
           item.istrue = "true";
         }
-        if(item.istrue=="true"){
-          sum+=(item.calories);
+        if (item.istrue == "true") {
+          sum += item.calories;
         }
-        total+=(item.calories);
-        
+        total += item.calories;
       });
       await data.save();
-      res.send({data:data.eveningsnacks,consumption:sum,total:total});
+      res.send({ data: data.eveningsnacks, consumption: sum, total: total });
     } else if (val == "dinner") {
-      let sum=0
-      let total=0
+      let sum = 0;
+      let total = 0;
       data.dinner.forEach((item) => {
         const x = JSON.stringify(item._id);
         const y = JSON.stringify(foodid);
         if (x == y) {
           item.istrue = "true";
         }
-        if(item.istrue=="true"){
-          sum+=(item.calories);
+        if (item.istrue == "true") {
+          sum += item.calories;
         }
-        total+=(item.calories);
-        
+        total += item.calories;
       });
       await data.save();
-      res.send({data:data.dinner,consumption:sum,total:total});
+      res.send({ data: data.dinner, consumption: sum, total: total });
     }
-    let value=0;
-    let total_calories=0
-    data.breakfast.forEach((el)=>{
-      if(el.istrue=="true"){
-        value+=el.calories
-      }else{
-        total_calories+=el.calories
+    let value = 0;
+    let total_calories = 0;
+    data.breakfast.forEach((el) => {
+      if (el.istrue == "true") {
+        value += el.calories;
+      } else {
+        total_calories += el.calories;
       }
-    })
-    data.lunch.forEach((el)=>{
-      if(el.istrue=="true"){
-        value+=el.calories
-      }else{
-        total_calories+=el.calories
+    });
+    data.lunch.forEach((el) => {
+      if (el.istrue == "true") {
+        value += el.calories;
+      } else {
+        total_calories += el.calories;
       }
-    })
-    data.eveningsnacks.forEach((el)=>{
-      if(el.istrue=="true"){
-        value+=el.calories
-      }else{
-        total_calories+=el.calories
+    });
+    data.eveningsnacks.forEach((el) => {
+      if (el.istrue == "true") {
+        value += el.calories;
+      } else {
+        total_calories += el.calories;
       }
-    })
-    data.dinner.forEach((el)=>{
-      if(el.istrue=="true"){
-        value+=el.calories
-      }else{
-        total_calories+=el.calories
+    });
+    data.dinner.forEach((el) => {
+      if (el.istrue == "true") {
+        value += el.calories;
+      } else {
+        total_calories += el.calories;
       }
-    })
+    });
 
-    const updateValue={
-      Targetcalories:total_calories+value,
-      Consumedcalories:value
-    }
-    await caloriesModel.findByIdAndUpdate({_id:Id},updateValue)
+    const updateValue = {
+      Targetcalories: total_calories + value,
+      Consumedcalories: value,
+    };
+    await caloriesModel.findByIdAndUpdate({ _id: Id }, updateValue);
   } catch (err) {
     console.log(err);
     res.send({ err: "Something went wrong" });
